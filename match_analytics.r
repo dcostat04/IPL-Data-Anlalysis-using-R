68abe3faadcecd7aec04a92ce3050f957b473551
Trevor Aaron Robert Dcosta <63400592+dcostat04@users.noreply.github.com>
2022-10-05 23:08:23 +0530

    read deliveries csv

--- match_analytics.r
+++ match_analytics.r
@@ -1,2 +1,2 @@
 matches = read.csv('C:/Users/Lenovo/Mini Project/BDA/IPLDataMatches/IPL Matches 2008-2020.csv');
-
+deliveries = read.csv('C:/Users/Lenovo/Mini Project/BDA/IPLDataMatches/IPL Ball-by-Ball 2008-2020.csv')
